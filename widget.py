606b139a7bf5f7b0b858c449ced609f07fdda45b
Antcating <dxtmvcqactxxdsokdf@protonmail.com>
2021-07-12 17:53:17 +0300

    Widget exception added

--- widget.py
+++ widget.py
@@ -11,16 +11,20 @@ def web_ring_gen(message, channel_link):
         channel_link = message.text
     else:
         channel_link = 'https://t.me/' + message.chat.username
-    current_channel_pos = channel_list.index(channel_link)
-    if current_channel_pos == len(channel_list) - 1:
-        next_channel = channel_list[0].replace('https://t.me/', '@')
-        previous_channel = channel_list[current_channel_pos - 1].replace('https://t.me/', '@')
-    elif current_channel_pos == 0:
-        next_channel = channel_list[current_channel_pos + 1].replace('https://t.me/', '@')
-        previous_channel = channel_list[-1].replace('https://t.me/', '@')
-    else:
-        next_channel = channel_list[current_channel_pos + 1].replace('https://t.me/', '@')
-        previous_channel = channel_list[current_channel_pos - 1].replace('https://t.me/', '@')
-    random_channel = channel_list[random.randint(0, len(channel_list) - 1)].replace('https://t.me/', '@')
-    list_channels = '@teleg_web_ring_list'
-    return next_channel, previous_channel, random_channel, list_channels
+    try:
+        current_channel_pos = channel_list.index(channel_link)
+        if current_channel_pos == len(channel_list) - 1:
+            next_channel = channel_list[0].replace('https://t.me/', '@')
+            previous_channel = channel_list[current_channel_pos - 1].replace('https://t.me/', '@')
+        elif current_channel_pos == 0:
+            next_channel = channel_list[current_channel_pos + 1].replace('https://t.me/', '@')
+            previous_channel = channel_list[-1].replace('https://t.me/', '@')
+        else:
+            next_channel = channel_list[current_channel_pos + 1].replace('https://t.me/', '@')
+            previous_channel = channel_list[current_channel_pos - 1].replace('https://t.me/', '@')
+        random_channel = channel_list[random.randint(0, len(channel_list) - 1)].replace('https://t.me/', '@')
+        list_channels = '@teleg_web_ring_list'
+        return next_channel, previous_channel, random_channel, list_channels
+    except ValueError:
+        print(channel_link, 'not in list')
+        return None, None, None, None
