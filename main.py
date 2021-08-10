43b4bcf07a41ad9235b8bc32c5e11dcbad0b53cd
Antcating <dxtmvcqactxxdsokdf@protonmail.com>
2021-08-10 14:23:54 +0300

    Interactive menu added

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import telebot, random
 import telebot as tb
 from widget import web_ring_gen
 
+main_menu = telebot.types.ReplyKeyboardMarkup()
+main_menu.row('➕ Add channel', '➖ Remove channel')
+main_menu.row('◻️ List channels', '✔️ Update config')
+
 
 def update_config():
     config.read('config.ini')
@@ -19,8 +23,8 @@ bot = tb.TeleBot(tb_token)
 def send_welcome(message):
     if str(message.from_user.id) in [admin_id.strip() for admin_id in config['AdminPanel']['ids'].split(',')]:
         bot.send_message(message.from_user.id, """
-Welcome to *Telegram Web Ring Control Bot*!
-Use /admin_help to get information about usage.""", parse_mode='Markdown')
+Welcome to <b>Telegram Web Ring Control Bot!</b>
+Use /admin_help to get information about usage.""", parse_mode='HTML', reply_markup=main_menu)
 
 
 @bot.message_handler(commands=['add'])
@@ -60,7 +64,7 @@ def admin_help(message):
     Due to Telegram restrictions, description of the channel can be up to 256 characters. WebRing uses at least 110-120 characters, so it is the second requirement to add the channel. Bot won't add channel, that doesn't have enough space in the description to create WebRing.
     IMPORTANT: Telegram has delay in updating info, so you will have to wait about 1-2 minutes, after changing description, to make a try to add channel.
 /remove - removing channel from the list and deleting bot from channel.
-/list - get list of the channels.''')
+/list - get list of the channels.''', reply_markup=main_menu)
 
 
 @bot.message_handler(commands=['remove'])
@@ -98,6 +102,7 @@ def list_channels(message):
             bot.send_message(message.from_user.id, 'Ring List is empty',
                              disable_web_page_preview=True)
 
+
 @bot.message_handler(commands=['update'])
 def config_update(message):
     if str(message.from_user.id) in [admin_id.strip() for admin_id in config['AdminPanel']['ids'].split(',')]:
@@ -105,11 +110,10 @@ def config_update(message):
         bot.send_message(message.from_user.id, 'Config updated')
 
 
-
 def channel_checking_system(message):
     channel_link = message.text.replace('https://t.me/', '@')
-    channel_info = bot.get_chat(channel_link)
     try:
+        channel_info = bot.get_chat(channel_link)
         activating_message = bot.send_message(channel_link,
                                               'Bot Ring Activating',
                                               disable_notification=True)
@@ -130,6 +134,7 @@ def channel_checking_system(message):
         elif telebot_error.result.status_code == 400:
             bot.send_message(message.from_user.id, 'Link incorrect or bot does not have enough permissions.')
         else:
+            print(telebot_error.result.content)
             print(telebot_error.result.content)
         return False
 
@@ -174,10 +179,10 @@ def update_ring_list():
         open('ring_list', 'w')
     if channel_list:
         current_list = bot.send_message(config['ChannelList']['link'],
-                                    '\n'.join(channel_list),
-                                    disable_notification=True,
-                                    disable_web_page_preview=True,
-                                    )
+                                        '\n'.join(channel_list),
+                                        disable_notification=True,
+                                        disable_web_page_preview=True,
+                                        )
         for id_list in range(int(current_list.id) - 3, int(current_list.id)):
             try:
                 bot.delete_message(config['ChannelList']['link'], id_list)
@@ -185,5 +190,15 @@ def update_ring_list():
                 print(update_error)
 
 
+@bot.message_handler(content_types=['text'])
+def reply_handler(message):
+    if message.text == '➕ Add channel':
+        channel_add(message)
+    elif message.text == '➖ Remove channel':
+        channel_delete(message)
+    elif message.text == '◻️ List channels':
+        list_channels(message)
+    elif message.text == '✔️ Update config':
+        config_update(message)
 update_ring_list()
 bot.polling(none_stop=True)
